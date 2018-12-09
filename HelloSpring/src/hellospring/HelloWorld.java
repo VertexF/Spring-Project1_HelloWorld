60251cfe34be77c5eda9b1e08a9b327e40ea0b42
VertexF <danmanley93@gmail.com>
2018-12-09 22:00:37 +0000

    Version 1
    Initial upload.

---
+++ HelloSpring/src/hellospring/HelloWorld.java
@@ -0,0 +1,16 @@
+
+package hellospring;
+
+public class HelloWorld 
+{
+   private String message;
+
+   public void setMessage(String message)
+   {
+      this.message  = message;
+   }
+   public void getMessage()
+   {
+      System.out.println("Your Message : " + message);
+   }
+}
